57f14239dd56a904f4540f0791b339524c7d9ddb
NataliaZolotareva <NataliaZolotareva@AKUVFA0026>
2024-05-13 00:40:40 -0400

    update

--- PracticeC#/SearchByTokens/Program.cs
+++ PracticeC#/SearchByTokens/Program.cs
@@ -17,14 +17,26 @@ class Solution
 {
     static void Main(string[] args)
     {
-        using var stdinStream = Console.OpenStandardInput();
-        using var stdin = new StreamReader(stdinStream);
-        var lines = stdin.ReadToEnd().Split(Environment.NewLine)
-            .Select(l =>
-            {
-                return l.Split(',');
-            })
-            .ToList();
+        //using var stdinStream = Console.OpenStandardInput();
+        //using var stdin = new StreamReader(stdinStream);
+        //var lines = stdin.ReadToEnd().Split(Environment.NewLine)
+        //    .Select(l =>
+        //    {
+        //        return l.Split(',');
+        //    })
+        //    .ToList();
+
+        //generate args to test 
+        var lines = new List<string[]>
+         {
+             new string[] { "weight", "FirstName", "100" },
+             new string[] { "weight", "LastName", "200" },
+             new string[] { "weight", "Address", "300" },
+             new string[] { "customer", "John", "Doe", "123 Main St" },
+             new string[] { "customer", "Jane", "Smith", "456 Elm St" },
+             new string[] { "customer", "Jennifer", "Potz", "333 4th South St" },
+             new string[] { "search", "Jennifer" }
+         };
 
         var weights = lines.Where(l => l[0] == "weight")
             .ToDictionary(x => x[1], x => int.Parse(x[2]));
@@ -42,6 +54,8 @@ class Solution
             .Select(l => l[1])
             .First();
 
+        Console.WriteLine($"search string: {searchString}");
+
         // perform the search
         var searchResults = Search(weights, customers, searchString);
 
@@ -50,17 +64,107 @@ class Solution
         {
             Console.WriteLine($"{result.Customer.FirstName} {result.Customer.LastName} {result.Customer.Address} - Score: {result.Score}");
         }
+        //keep console app running until key press
+        Console.ReadKey();
     }
 
     static List<SearchResult> Search(Dictionary<string, int> weights, List<Customer> customers, string searchString)
     {
         var results = new List<SearchResult>();
+        var searchTokens = TokenizeSearchString(searchString);
 
-        // TODO: Add your search implementation here
-        throw new NotImplementedException();
+        foreach (var customer in customers)
+        {
+            int score = 0;
+
+            //if full match by token then return weight 400% per field
+            CheckFullMatch(customer, searchTokens, weights, ref score);
+
+            if (score == 0)
+            {
+                //if partial match by token then return weight 200% per field
+                CheckPartialMatch(customer, searchTokens, weights, ref score);
+
+                if (score == 0)
+                {
+                    //if no partial match then check for any match
+                    CheckAnyMatch(customer, searchTokens, weights, ref score);
+                }
+            }
+
+            if (score > 0)
+            {
+                results.Add(new SearchResult
+                {
+                    Customer = customer,
+                    Score = score
+                });
+            }
+        }
 
         // return your results
-        return results;
+        return results.OrderByDescending(r => r.Score).Take(5).ToList();
+    }
+
+    private static void CheckFullMatch(Customer customer, List<string> tokens, Dictionary<string, int> weights, ref int score)
+    {
+        foreach (var token in tokens)
+        {
+            //if token exact match to customer first name then return 400% weight
+            if (token.Equals(customer.FirstName))
+            {
+                score += weights["FirstName"] * 4;
+            }
+            //if token exact match to customer last name then return 400% weight
+            if (token.Equals(customer.LastName))
+            {
+                score += weights["LastName"] * 4;
+            }
+            //if token exact match to customer address then return 400% weight
+            if (token.Equals(customer.Address))
+            {
+                score += weights["Address"] * 4;
+            }
+        }
+    }
+
+    private static void CheckPartialMatch(Customer customer, List<string> tokens, Dictionary<string, int> weights, ref int score)
+    {
+        foreach (var token in tokens)
+        {
+            //if token matches anywhere in search string then return 100% of weight
+            if (customer.FirstName.Contains(token))
+            {
+                score += weights["FirstName"];
+            }
+            if (customer.LastName.Contains(token))
+            {
+                score += weights["LastName"];
+            }
+            if (customer.Address.Contains(token))
+            {
+                score += weights["Address"];
+            }
+        }
+    }
+
+    private static void CheckAnyMatch(Customer customer, List<string> tokens, Dictionary<string, int> weights, ref int score)
+    {
+        //if token partial match to customer first name then return 200% weight
+        if (tokens.Contains(customer.FirstName))
+        {
+            score += weights["FirstName"] * 2;
+        }
+        //if token partial match to customer last name then return 200% weight
+        if (tokens.Contains(customer.LastName))
+        {
+            score += weights["LastName"] * 2;
+        }
+        //if token partial match to customer address then return 200% weight
+        if (tokens.Contains(customer.Address))
+        {
+            score += weights["Address"] * 2;
+        }
     }
 
     static List<string> TokenizeSearchString(string searchString)
